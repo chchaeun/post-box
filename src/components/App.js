e55520a543f907da65d6bee9c55f4bba8436789c
Kim Chae Eun <kce3615@naver.com>
2021-08-18 23:31:18 +0900

    add userobj

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import {authService} from 'fBase';
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
 
   useEffect(()=>{
     authService.onAuthStateChanged(user =>{
       if(user){
         setIsLoggedIn(true);
+        setUserObj(user);
       }else{
         setIsLoggedIn(false);
       }
@@ -18,7 +20,7 @@ function App() {
 
   return (
     <>
-      {init? <AppRouter isLoggedIn={isLoggedIn} />:"Loading..."}
+      {init? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>:"Loading..."}
       <footer>&copy; {new Date().getFullYear()} Cwitter</footer>
     </>
   );
