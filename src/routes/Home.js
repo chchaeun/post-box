d6cb03efac2fdbe77630803f7c32571a9f7d9557
Kim Chae Eun <kce3615@naver.com>
2021-08-27 15:03:08 +0900

    add css

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import 'styles/Home.css'
 import 'bootstrap/dist/css/bootstrap.css';
-
+import { useHistory } from "react-router-dom";
+import {GoMail} from 'react-icons/go'
+import {BsPencil} from 'react-icons/bs'
 const Home = ({userObj})=>{
+
+    const history = useHistory();
+
     const [friendId, setFriendId] = useState();
 
     const onChange = (event)=>{
@@ -12,25 +17,31 @@ const Home = ({userObj})=>{
 
     const onSubmit= (event)=>{
         event.preventDefault();
-        window.location.replace(`/${friendId}`);
+        history.push(`/${friendId}`);
     }
 
     const onMyPost = ()=>{
-        window.location.replace(`/${userObj.uid}`);
+        history.push(`/${userObj.uid}`);
     }
 
     return(
         <div id="container">
-            <form onSubmit={onSubmit} className="form-group">
-                <label for="friend-box">Friend's Post Box</label>
+            <button id="my-box" type="button" onClick={onMyPost}>
+            <GoMail size="24" color="rgb(90, 90, 90)"/>
+            <p style={{"padding":"10px"}}>내 우편함 가기</p></button>
+
+            <form onSubmit={onSubmit} id="friend-form" className="form-group">
+                <BsPencil size="24" color="rgb(90, 90, 90)" style={{"margin-left":"59px"}}/><br/>
+                <label for="friend-box">친구의 우편함 가기</label>
                 <input name="friend-box" 
                 className="form-control" 
                 type="text" 
                 value={friendId} 
-                onChange={onChange}/>
-                <input id="friend-submit" type="submit" value="submit" className="btn btn-outline-secondary"/>
+                onChange={onChange}
+                placeholder="코드 입력"/>
+                <input id="friend-submit" type="submit" value="방문하기" 
+                className="btn btn-outline-secondary"/>
             </form>
-            <button type="button" onClick={onMyPost} className="btn btn-outline-secondary">My Post Box</button>
         </div>
     );
 }
