e901d1f1e5f4c0d8dd4d50e154d5244fdadaee5b
Kim Chae Eun <kce3615@naver.com>
2021-08-28 15:03:47 +0900

    modify router

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from 'routes/Home';
 import Auth from 'routes/Auth';
 import Profile from 'routes/Profile';
@@ -8,14 +8,14 @@ import PostBox from 'routes/PostBox';
 
 const AppRouter = ({refreshUser, isLoggedIn, userObj}) =>{
     return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {isLoggedIn && <Navigation userObj={userObj}/>}
       <Switch>
         <Route exact path="/">{isLoggedIn ? <Home userObj={userObj}/>:<Auth/>}</Route>
         <Route exact path="/profile">{isLoggedIn ? <Profile refreshUser={refreshUser} userObj={userObj}/>:<Auth/>}</Route>
         <Route exact path="/:id">{isLoggedIn ? <PostBox userObj={userObj}/>:<Auth/>}</Route>
       </Switch>
-    </Router>
+    </BrowserRouter>
     );
 }
 
