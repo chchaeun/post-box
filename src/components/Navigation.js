a7f2e0b5604cfd570fe07de46a3aa157cc301e1e
Kim Chae Eun <kce3615@naver.com>
2021-08-28 15:35:32 +0900

    add router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,18 +3,28 @@ import {AiOutlineUser} from "react-icons/ai"
 import 'styles/Home.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
+import {Link, useHistory} from 'react-router-dom'
+
 const Navigation = ({userObj}) =>{
 
+    const history = useHistory();
+
+    const onHomeClick = ()=>{
+        history.push("/");
+    }
+    const onProfileClick = ()=>{
+        history.push('/profile')
+    }
     return (
         <>
             <h2>Post-Box</h2>
             <nav>
                 <ul>
                     <li>
-                        <a href="/"><FiInbox id="icon"/>Home</a>
+                        <span type="button" onClick={onHomeClick}><FiInbox id="icon"/>Home</span>
                     </li>
                     <li>
-                        <a href="/profile"><AiOutlineUser id="icon"/>{userObj.displayName}</a>
+                        <span type="button" onClick={onProfileClick}><AiOutlineUser id="icon"/>{userObj.displayName}</span>
                     </li>
                 </ul>
             </nav>
